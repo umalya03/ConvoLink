d42ab8d54a93949dfa4b78aa4412ca448e8a626e
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-06-19 10:03:43 +0530

    Setting up and optimizing codebase

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import"dotenv/config";
+
+import authRoutes from "./routes/auth.route.js";
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
