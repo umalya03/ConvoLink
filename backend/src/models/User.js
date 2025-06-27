e2a712bf88be4a3cd277ff2ec73085bddcadf4fb
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-06-27 12:01:22 +0530

    Login, logout endpoints and Create stream user

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -61,7 +61,13 @@ userSchema.pre("save", async function(next) {
     } catch (error) {
         next(error);
     }
-})
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+
+    const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+    return isPasswordCorrect;
+}
 
 const User = mongoose.model("User", userSchema);
 
