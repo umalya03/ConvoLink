ee1e42de22fe95a83552fa50cced1c3da7d54ce8
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-06-27 15:21:10 +0530

    Onboarding endpoint

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router()
 
@@ -7,4 +8,10 @@ router.post("/signup" , signup);
 router.post("/login" , login);
 router.post("/logout" , logout);
 
+router.post("/onboarding", protectRoute, onboard);
+
+//check if user is logged in
+router.get("/me", protectRoute, (req,res) => {
+    res.status(200).json({success: true, user: req.user});
+})
 export default router;
