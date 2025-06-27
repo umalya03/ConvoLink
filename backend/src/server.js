cee66058ba13e4c13314b69997abba8596e32d2d
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-06-27 17:29:26 +0530

    Chat route

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js";
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
