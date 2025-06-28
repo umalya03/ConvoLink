d131ee3a0f934ba0292ebec638cba7ace490954b
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-06-28 16:04:30 +0530

    Onboarding Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,46 +7,55 @@ import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 
-import {Toaster} from "react-hot-toast";
-import {useEffect , useState} from 'react';
-import {useQuery} from "@tanstack/react-query";
-import {axiosInstance} from "./lib/axios.js";
+import { Toaster } from "react-hot-toast";
+import PageLoader from './components/PageLoader.jsx';
+import useAuthUser from './hooks/useAuthUser.js';
 
 
 const App = () => {
-  //tanstack query
-  const {data:authData, isLoading,error} = useQuery({
-    queryKey:["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false, //auth check
-  });
- 
-  const authUser = authData?.user
 
-  return ( 
-  <div className='h-screen' data-theme="night">
-    <Routes>
-      <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-      <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-      <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-      <Route 
-        path="/notifications" 
-        element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} 
-      />
-      <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login" />} />
-      <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
-      <Route 
-        path="/onboarding" 
-        element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} 
-      />
+  const { isLoading, authUser } = useAuthUser();
 
-    </Routes>
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded
 
-    <Toaster />
-  </div>
-)};
+  if (isLoading) return <PageLoader />;
+
+  return (
+    <div className='h-screen' data-theme="autumn">
+      <Routes>
+        <Route path="/" element={isAuthenticated && isOnboarded ? (
+          <HomePage />
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )}
+        />
+        <Route path="/signup" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />} />
+        <Route
+          path="/notifications"
+          element={isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />}
+        />
+        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route
+          path="/onboarding"
+          element={isAuthenticated ? (
+            !isOnboarded ? (
+              <OnboardingPage />
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          )}
+        />
+
+      </Routes>
+
+      <Toaster />
+    </div>
+  )
+};
 
 export default App
