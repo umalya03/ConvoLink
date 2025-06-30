db79649632545fcebe78405d446dee516a66cb55
Viduni Umalya <dedimunigeviduni@gmail.com>
2025-06-30 13:44:00 +0530

    Video call page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,12 @@ const App = () => {
             <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
           )}
         />
-        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/call/:id" element={isAuthenticated && isOnboarded ? ( 
+        <CallPage /> 
+        ) : (
+        <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )
+      } />
         
         <Route path="/chat/:id" element={isAuthenticated && isOnboarded ? (
           <Layout showSidebar={false}>
